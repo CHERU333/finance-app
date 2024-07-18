a4cf9569dfef5329121c3392eb135d82221c5b0d
Fujii Rise <pi@cheru.jp>
2024-07-18 17:00:54 +0900

    Authentication ok

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,29 @@
-const SignUpPage = () => {
-    return (
-        <div>
-            Sign Up Page    
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
+export default function Page() {
+    return(
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+            <div className="h-full lg:flex flex-col items-center justify-center scroll-px-40">
+                <div className="text-center space-y-4 pt-16">
+                    <h1 className="text-3xl font-bold">Welcom Back!</h1>
+                    <p className="text-base text-[#7E8CA0]">
+                        Log in or Create account to get back to your dashboard!
+                    </p>
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                    <ClerkLoaded>
+                        <SignUp path="/sign-up" />
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className="animate-spin text-muted" />
+                    </ClerkLoading>
+                </div>
+            </div>
+            <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+                <Image src="/logo.svg" width={100} height={100}  alt="Logo"/>
+            </div>
         </div>
     );
 }
-export default SignUpPage;
